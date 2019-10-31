a14be963009092f0ec4915f11f107a0bb976d24d
Amulya Reddy Konda <amulyareddyk@yahoo.in>
2019-10-31 10:09:19 +0530

    Traffic detection

---
+++ object_detection.py
@@ -0,0 +1,14 @@
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "traffic.mp4"),
+                            output_file_path=os.path.join(execution_path, "traffic_detected")
+                            , frames_per_second=20, log_progress=True)
+print(video_path)
