5d314be1058397cba04724ba93e1317a44080028
AmulyaReddy99 <amulyareddyk@yahoo.in>
2019-12-10 17:03:29 +0530

    navigation added

--- Django app/outputs/load_saved_model.py
+++ Django app/outputs/load_saved_model.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2,os
 
-from imageai.Detection import VideoObjectDetection
+# from imageai.Detection import VideoObjectDetection
 
 # @Todo
 ''' Remove unnecessary imports'''
